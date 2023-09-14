2ac494215b2792aee1a52ba355bd359537fd9703
ゆっくりたく <taku565758@gmail.com>
2023-09-14 19:50:49 +0900

    MaidExtensionからマイクラバージョンを取得するように

--- src/main/java/io/github/yukkuritaku/maidgradle/loom/task/DownloadLittleMaidJarTask.java
+++ src/main/java/io/github/yukkuritaku/maidgradle/loom/task/DownloadLittleMaidJarTask.java
@@ -1,7 +1,6 @@
 package io.github.yukkuritaku.maidgradle.loom.task;
 
 import io.github.yukkuritaku.maidgradle.loom.util.MaidConstants;
-import net.fabricmc.loom.configuration.providers.minecraft.MinecraftVersionMeta;
 import net.fabricmc.loom.util.download.GradleDownloadProgressListener;
 import net.fabricmc.loom.util.gradle.ProgressGroup;
 import org.gradle.api.tasks.TaskAction;
@@ -17,23 +16,23 @@ public class DownloadLittleMaidJarTask extends AbstractMaidTask{
     @TaskAction
     public void downloadJars(){
         getProject().getLogger().lifecycle("Download LittleMaid Dependencies...");
-        MinecraftVersionMeta versionInfo = getLoomExtension().getMinecraftProvider().getVersionInfo();
+        String versionInfo = getMaidExtension().getMinecraftVersion().get();
         try(ProgressGroup progressGroup = new ProgressGroup(getProject(), "Download LittleMaidModelLoader")) {
             getLoomExtension()
-                    .download(MaidConstants.LittleMaidJarFileUrls.getLMMLDownloadUrl(versionInfo.id(), getMaidExtension()))
+                    .download(MaidConstants.LittleMaidJarFileUrls.getLMMLDownloadUrl(versionInfo, getMaidExtension()))
                     .progress(new GradleDownloadProgressListener("LittleMaidModelLoader", progressGroup::createProgressLogger))
                     .downloadPath(getMaidExtension().getLMMLOutputDirectory().get().file(
-                            "LMML-" + versionInfo.id() + "-" + getMaidExtension().getLittleMaidModelLoaderVersion().get() + "-Fabric.jar"
+                            "LMML-" + versionInfo + "-" + getMaidExtension().getLittleMaidModelLoaderVersion().get() + "-Fabric.jar"
                     ).getAsFile().toPath());
         }catch (IOException e){
             throw new RuntimeException(e);
         }
         try(ProgressGroup progressGroup = new ProgressGroup(getProject(), "Download LittleMaidReBirth")) {
             getLoomExtension()
-                    .download(MaidConstants.LittleMaidJarFileUrls.getLMRBDownloadUrl(versionInfo.id(), getMaidExtension()))
+                    .download(MaidConstants.LittleMaidJarFileUrls.getLMRBDownloadUrl(versionInfo, getMaidExtension()))
                     .progress(new GradleDownloadProgressListener("LittleMaidReBirth", progressGroup::createProgressLogger))
                     .downloadPath(getMaidExtension().getLMRBOutputDirectory().get().file(
-                            "LMRB-" + versionInfo.id() + "-" + getMaidExtension().getLittleMaidReBirthVersion().get() + "-Fabric.jar"
+                            "LMRB-" + versionInfo + "-" + getMaidExtension().getLittleMaidReBirthVersion().get() + "-Fabric.jar"
                     ).getAsFile().toPath());
         }catch (IOException e){
             throw new RuntimeException(e);
