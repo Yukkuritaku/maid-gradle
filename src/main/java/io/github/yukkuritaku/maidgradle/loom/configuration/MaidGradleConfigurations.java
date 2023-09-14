bbed3ef779e96944825733d46914240e1079242b
ゆっくりたく <taku565758@gmail.com>
2023-09-14 14:56:50 +0900

    もろっといろいろ変更

--- src/main/java/io/github/yukkuritaku/maidgradle/loom/configuration/MaidGradleConfigurations.java
+++ src/main/java/io/github/yukkuritaku/maidgradle/loom/configuration/MaidGradleConfigurations.java
@@ -2,7 +2,10 @@ package io.github.yukkuritaku.maidgradle.loom.configuration;
 
 import io.github.yukkuritaku.maidgradle.loom.MaidGradleExtension;
 import io.github.yukkuritaku.maidgradle.loom.util.MaidConstants;
+import net.fabricmc.loom.configuration.LoomConfigurations;
+import org.gradle.api.NamedDomainObjectProvider;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
@@ -26,6 +29,8 @@ public abstract class MaidGradleConfigurations implements Runnable{
     @Override
     public void run() {
         MaidGradleExtension maidGradleExtension = MaidGradleExtension.get(getProject());
+        register(MaidConstants.Configurations.LITTLE_MAID_MODEL_LOADER, Role.RESOLVABLE);
+        register(MaidConstants.Configurations.LITTLE_MAID_REBIRTH, Role.RESOLVABLE);
         extendsFrom(MaidConstants.Configurations.FABRIC_MOD_IMPLEMENTATION, MaidConstants.Configurations.LITTLE_MAID_MODEL_LOADER);
         extendsFrom(MaidConstants.Configurations.FABRIC_MOD_IMPLEMENTATION, MaidConstants.Configurations.LITTLE_MAID_REBIRTH);
         getRepositories().flatDir(flatDirectoryArtifactRepository ->
@@ -40,7 +45,30 @@ public abstract class MaidGradleConfigurations implements Runnable{
                 MaidConstants.Dependencies.getLittleMaidReBirth(getProject()));
     }
 
+    private NamedDomainObjectProvider<Configuration> register(String name, Role role) {
+        return getConfigurations().register(name, role::apply);
+    }
+
     public void extendsFrom(String a, String b) {
         getConfigurations().getByName(a, configuration -> configuration.extendsFrom(getConfigurations().getByName(b)));
+    }
+
+    enum Role {
+        NONE(false, false),
+        CONSUMABLE(true, false),
+        RESOLVABLE(false, true);
+
+        private final boolean canBeConsumed;
+        private final boolean canBeResolved;
+
+        Role(boolean canBeConsumed, boolean canBeResolved) {
+            this.canBeConsumed = canBeConsumed;
+            this.canBeResolved = canBeResolved;
+        }
+
+        void apply(Configuration configuration) {
+            configuration.setCanBeConsumed(canBeConsumed);
+            configuration.setCanBeResolved(canBeResolved);
+        }
     }
 }
