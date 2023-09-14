463e969b1f1ca4768da4279cae420fc3eac5dba5
ゆっくりたく <taku565758@gmail.com>
2023-09-14 15:21:11 +0900

    gradle変更

--- src/main/java/io/github/yukkuritaku/maidgradle/loom/task/BuildLittleMaidModelZipTask.java
+++ src/main/java/io/github/yukkuritaku/maidgradle/loom/task/BuildLittleMaidModelZipTask.java
@@ -8,19 +8,24 @@ import org.gradle.api.tasks.bundling.Zip;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import java.io.File;
 
 public abstract class BuildLittleMaidModelZipTask extends Zip {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BuildLittleMaidModelZipTask.class);
 
+
+
     @OutputDirectory
     public abstract RegularFileProperty getLittleMaidModelOutputDir();
 
     @Input
-    public abstract SourceSetOutput getSourceSetOutputDir();
+    public SourceSetOutput getSourceSetOutputDir(){
+        return SourceSetHelper.getMainSourceSet(getProject()).getOutput();
+    }
 
-
+    @Inject
     public BuildLittleMaidModelZipTask() {
         super();
         setGroup(MaidConstants.MAID_GRADLE);
@@ -33,14 +38,9 @@ public abstract class BuildLittleMaidModelZipTask extends Zip {
     public void run() {
         String baseName = getArchiveBaseName().get();
         getArchiveBaseName().set("littleMaidMob-" + baseName + "-" + getArchiveVersion().get());
-        if (getSourceSetOutputDir() == null) {
-            from(SourceSetHelper.getMainSourceSet(getProject()));
-            LOGGER.info("Project sourceSet: main(default)");
-        }else {
-            from(getSourceSetOutputDir());
-            String[] outputDir = getSourceSetOutputDir().getClassesDirs().getAsPath().split("[/\\\\]");
-            LOGGER.info("Project sourceSet: {}(custom)", outputDir[outputDir.length - 1]);
-        }
+        from(getSourceSetOutputDir());
+        String[] outputDir = getSourceSetOutputDir().getClassesDirs().getAsPath().split("[/\\\\]");
+        LOGGER.info("Project sourceSet: {}(custom)", outputDir[outputDir.length - 1]);
     }
 
 
