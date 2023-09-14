2b77065710c91a59292db7e9c4f85795e22f2a5c
ゆっくりたく <taku565758@gmail.com>
2023-09-15 01:13:55 +0900

    色々変更

--- src/main/java/io/github/yukkuritaku/maidgradle/loom/MaidGradlePlugin.java
+++ src/main/java/io/github/yukkuritaku/maidgradle/loom/MaidGradlePlugin.java
@@ -3,13 +3,22 @@ package io.github.yukkuritaku.maidgradle.loom;
 import io.github.yukkuritaku.maidgradle.loom.extension.MaidGradleExtension;
 import io.github.yukkuritaku.maidgradle.loom.task.BuildLittleMaidModelZipTask;
 import io.github.yukkuritaku.maidgradle.loom.task.DownloadLittleMaidJarTask;
+import net.fabricmc.loom.LoomGradleExtension;
 import net.fabricmc.loom.bootstrap.BootstrappedPlugin;
+import net.fabricmc.loom.util.Checksum;
+import net.fabricmc.loom.util.gradle.GradleUtils;
+import net.fabricmc.loom.util.service.ScopedSharedServiceManager;
+import net.fabricmc.loom.util.service.SharedServiceManager;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.PluginAware;
 import org.gradle.api.tasks.TaskContainer;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class MaidGradlePlugin implements BootstrappedPlugin {
 
@@ -28,9 +37,6 @@ public class MaidGradlePlugin implements BootstrappedPlugin {
             var downloadLittleMaidJars = tasks.register("downloadLittleMaidJars", DownloadLittleMaidJarTask.class, task -> {
                 task.setDescription("Download LittleMaid Jar from dropbox.");
             });
-            project.getLogger().lifecycle(maidGradleExtension.getMinecraftVersion().get());
-            project.getLogger().lifecycle(maidGradleExtension.getLittleMaidModelLoaderVersion().get());
-            project.getLogger().lifecycle(maidGradleExtension.getLittleMaidReBirthVersion().get());
             downloadLittleMaidJars.configure(downloadLittleMaidJarTask -> {
                 downloadLittleMaidJarTask.getMinecraftVersion().set(maidGradleExtension.getMinecraftVersion());
                 downloadLittleMaidJarTask.getLittleMaidModelLoaderVersion().set(maidGradleExtension.getLittleMaidModelLoaderVersion());
@@ -45,6 +51,76 @@ public class MaidGradlePlugin implements BootstrappedPlugin {
                         );
                     }
             ));
+            afterEvaluationWithService(project, sharedServiceManager -> {
+                project.getLogger().info(":setting up littlemaid dependencies");
+                final LoomGradleExtension extension = LoomGradleExtension.get(project);
+                final boolean previousRefreshDeps = extension.refreshDeps();
+                if (getAndLock(project)) {
+                    project.getLogger().lifecycle("Found existing cache lock file, rebuilding loom cache. This may have been caused by a failed or canceled build.");
+                    extension.setRefreshDeps(true);
+                }
+                try {
+                    downloadLittleMaidJars.get().downloadJars();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                extension.getDependencyManager().handleDependencies(project, sharedServiceManager);
+                releaseLock(project);
+                extension.setRefreshDeps(previousRefreshDeps);
+            });
         }
+    }
+    private Path getLockFile(Project project) {
+        final LoomGradleExtension extension = LoomGradleExtension.get(project);
+        final Path cacheDirectory = extension.getFiles().getUserCache().toPath();
+        final String pathHash = Checksum.projectHash(project);
+        return cacheDirectory.resolve("." + pathHash + ".lock");
+    }
+
+    private boolean getAndLock(Project project) {
+        final Path lock = getLockFile(project);
+
+        if (Files.exists(lock)) {
+            return true;
+        }
+
+        try {
+            Files.createFile(lock);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to acquire getProject() configuration lock", e);
+        }
+
+        return false;
+    }
+
+    private void releaseLock(Project project) {
+        final Path lock = getLockFile(project);
+
+        if (!Files.exists(lock)) {
+            return;
+        }
+
+        try {
+            Files.delete(lock);
+        } catch (IOException e1) {
+            try {
+                // If we failed to delete the lock file, moving it before trying to delete it may help.
+                final Path del = lock.resolveSibling(lock.getFileName() + ".del");
+                Files.move(lock, del);
+                Files.delete(del);
+            } catch (IOException e2) {
+                var exception = new UncheckedIOException("Failed to release getProject() configuration lock", e2);
+                exception.addSuppressed(e1);
+                throw exception;
+            }
+        }
+    }
+
+    private void afterEvaluationWithService(Project project, Consumer<SharedServiceManager> consumer) {
+        GradleUtils.afterSuccessfulEvaluation(project, () -> {
+            try (var serviceManager = new ScopedSharedServiceManager()) {
+                consumer.accept(serviceManager);
+            }
+        });
     }
 }
